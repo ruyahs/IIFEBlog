bc24c49f0b3053f7483282542a3187eb0c828a93
ruyahs <shayurb@gmail.com>
2021-11-26 20:52:33 +0200

    Blog for meduim

---
+++ Index.js
@@ -0,0 +1,12 @@
+var RangeOfValues = ((low , high) => {
+    var ArrOfRangeOFValues = [] ; 
+    for (let index = low; index < high; index++) {
+        ArrOfRangeOFValues.push(index)
+        
+    }
+
+    return ArrOfRangeOFValues ; 
+
+})(0,100) ; 
+
+console.log(RangeOfValues);
